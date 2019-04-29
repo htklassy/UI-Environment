e63dd02316740d56fe429371114bb7760bdc8a65
htklassy <htklassy@gmail.com>
2019-04-29 12:44:11 -0600

    Add files via upload

---
+++ Assets/Scripts/SFX.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour
+{
+
+    public AudioSource Clap;
+    public AudioSource Kick;
+    public AudioSource HiHat;
+    public AudioSource VocalStab;
+    public AudioSource Shaker;
+    public AudioSource HarpOne;
+    public AudioSource HarpTwo;
+    public AudioSource HarpThree;
+    public AudioSource SampleOne;
+    public AudioSource SampleTwo;
+    public AudioSource SampleThree;
+    public AudioSource DeepKnight;
+    
+
+    public void PlayClap()
+    {
+        Clap.Play();
+    }
+    public void PlayKick()
+    {
+        Kick.Play();
+    }
+    public void PlayHiHat()
+    {
+        HiHat.Play();
+    }
+    public void PlayVocalStab()
+    {
+        VocalStab.Play();
+    }
+    public void PlayShaker()
+    {
+        Shaker.Play();
+    }
+    public void PlayHarpOne()
+    {
+        HarpOne.Play();
+    }
+    public void PlayHarpTwo()
+    {
+        HarpTwo.Play();
+    }
+    public void PlayHarpThree()
+    {
+        HarpThree.Play();
+    }
+    public void PlaySampleOne()
+    {
+        SampleOne.Play();
+    }
+    public void PlaySampleTwo()
+    {
+        SampleTwo.Play();
+    }
+    public void PlaySampleThree()
+    {
+        SampleThree.Play();
+    }
+    public void PlayDeepKnight()
+    {
+        DeepKnight.Play();
+    }
+  
+}
